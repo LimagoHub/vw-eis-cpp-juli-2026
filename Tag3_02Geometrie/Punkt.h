8a2bc8cb8c98ba0619bc6f4525339c21a2e77544
Jo Wagner <jowagner@limago.de>
2026-07-15 09:19:29 +0200

    initial commit

---
+++ Tag3_02Geometrie/Punkt.h
@@ -0,0 +1,10 @@
+//
+// Created by JoachimWagner on 15.07.2026.
+//
+
+#ifndef TAG3_02GEOMETRIE_PUNKT_H
+#define TAG3_02GEOMETRIE_PUNKT_H
+
+// Hier kommt die Klasse rein
+
+#endif //TAG3_02GEOMETRIE_PUNKT_H
